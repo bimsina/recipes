47814ebfb50854fa6d9aba8927143c7d6d3b4147
Bibek Timsina <timsinabibek960@gmail.com>
2021-09-05 09:41:44 +0545

    web support added

---
+++ android/app/src/main/kotlin/com/bimsina/recipes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bimsina.recipes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Nepali Recipes',
+      title: 'Recipes',
       theme: ThemeData(
         primaryColor: Color(0xff263341),
         accentColor: Color(0xff8DB646),
